6dba1bf7c59023eeb9fbe58cbae45ff9d94fb9fa
BetelTagesse <BetelTagesse367@gmail.com>
2025-11-25 15:34:12 +0300

    products backend done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -112,3 +112,45 @@ export const getRecommendedProducts = async (req, res) => {
     });
   }
 };
+
+export const getProductsByCategory = async (req, res) => {
+  const { category } = req.params;
+
+  try {
+    const products = await Product.find({ category });
+    res.status(200).json({ products });
+  } catch (error) {
+    console.error("Error fetching products by category:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updatedProduct = await product.save();
+
+      //update redis cache
+      await updateFeaturedProductsCache();
+
+      res
+        .status(200)
+        .json({ message: "Product featured status updated", updatedProduct });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.log("Error in toggleFeaturedProduct controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+const updateFeaturedProductsCache = async () => {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+  } catch (error) {
+    console.log("Error updating featured products cache", error.message);
+  }
+};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -4,6 +4,7 @@ import {
   deleteProduct,
   getAllProducts,
   getFeaturedProducts,
+  getProductsByCategory,
   getRecommendedProducts,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
@@ -12,8 +13,11 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
