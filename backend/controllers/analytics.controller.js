c3c63dbd9a8020d070e7aaabf0c40b14aba72849
BetelTagesse <BetelTagesse367@gmail.com>
2025-11-28 16:37:03 +0300

    date analytics  done

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,17 +1,77 @@
+import Order from "../models/order.model";
 import Product from "../models/product.model";
 import User from "../models/user.model";
 
 export const getAnalyticsData = async () => {
-  const totalUser = await User.countDocuments();
+  const totalUsers = await User.countDocuments();
   const totalProducts = await Product.countDocuments();
 
   const salesData = await Order.aggregate([
     {
       $group: {
         _id: null,
-        totalSales: { $sum: "$totalAmount" },
-        totalOrders: { $sum: 1 },
+        totalSales: { $sum: "$1" },
+        totalRevenue: { $sum: "$totalAmount" },
       },
     },
   ]);
+  const { totalSales, totalRevenue } = salesData[0] || {
+    totalSales: 0,
+    totalRevenue: 0,
+  };
+
+  return {
+    users: totalUsers,
+    products: totalProducts,
+    totalSales,
+    totalRevenue,
+  };
 };
+
+export const getDailySalesData = async (startDate, endDate) => {
+  const dailySalesData = await Order.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+        sales: { $sum: 1 },
+        revenue: {
+          $sum: "$totalAmount",
+        },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ]);
+
+  const dateArray = getDatesInRange(startDate, endDate);
+  //  console.log(dateArray);
+
+  return dateArray.map((date) => {
+    const foundData = dailySalesData.find((item) => item._id === date);
+
+    return {
+      date,
+      sales: foundData?.sales || 0,
+      revenue: foundData?.revenue || 0,
+    };
+  });
+};
+
+function getDatesInRange(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
--- backend/routes/analytics.routes.js
+++ backend/routes/analytics.routes.js
@@ -1,12 +1,26 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware";
+import { getAnalyticsData } from "../controllers/analytics.controller";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, async (req, res) => {
   try {
     const analyticsData = await getAnalyticsData();
-  } catch (error) {}
+
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const dailySalesData = await getDailySalesData(startDate, endDate);
+
+    res.json({
+      analyticsData,
+      dailySalesData,
+    });
+  } catch (error) {
+    console.log("error in analytics route");
+    res.status(500).json({ message: "server error", error: error.message });
+  }
 });
 
 export default router;
